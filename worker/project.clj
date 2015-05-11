967b862292bc6c4956ea59ec06992db35d901469
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2015-05-11 18:49:17 +0200

    friboo update

--- worker/project.clj
+++ worker/project.clj
@@ -8,7 +8,7 @@
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.zalando.stups/friboo "0.10.0"]]
+                 [org.zalando.stups/friboo "0.11.0"]]
 
   :main ^:skip-aot org.zalando.stups.mint.worker.core
   :uberjar-name "mint-worker.jar"
