3f2540adfc161402fa142d1c5b7d363990e8ac2d
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-08-27 10:59:53 +0200

    #5 fix conpath and tests

--- src/org/zalando/stups/mint/worker/util.clj
+++ src/org/zalando/stups/mint/worker/util.clj
@@ -1,14 +1,25 @@
-(ns org.zalando.stups.mint.worker.util)
+(ns org.zalando.stups.mint.worker.util
+  (:require [clojure.string :as s]))
+
+(defmacro xor
+  [a b]
+  `(or (and ~a (not ~b))
+       (and (not ~a) ~b)))
 
 (defn conpath
   "Concatenates path elements to an URL."
   [url & path]
   (let [[x & xs] path]
     (if x
-      (let [url (if (or
-                      (.endsWith url "/")
-                      (.startsWith x "/"))
+      (let [url (if (xor (.endsWith url "/")
+                         (.startsWith x "/"))
+                  ; concat if exactly one of them has a /
                   (str url x)
-                  (str url "/" x))]
+                  (if (and (.endsWith url "/")
+                           (.startsWith x "/"))
+                    ; if both have a /, remove one
+                    (str url (s/replace-first x #"/" ""))
+                    ; if none have a /, add one
+                    (str url "/" x)))]
         (recur url xs))
       url)))
