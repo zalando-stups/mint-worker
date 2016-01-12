ec9601853054e3604494f8d891f1815d0cb0659c
Henning Jacobs <henning@jacobs1.de>
2016-01-12 12:17:07 +0100

    add a trivial test

---
+++ test/org/zalando/stups/mint/worker/external/etcd_test.clj
@@ -0,0 +1,22 @@
+(ns org.zalando.stups.mint.worker.external.etcd-test
+  (:require [clojure.test :refer :all]
+            [clj-http.client :as client]
+            [org.zalando.stups.mint.worker.external.etcd :as etcd]))
+
+(defn mock-put-error
+  [& args]
+  (throw (Exception. "ohmigods")))
+
+(defn mock-success
+  [& args]
+  {:some "result"})
+
+(deftest etcd-failure-false
+  (with-redefs [client/put mock-put-error]
+    (is (= (etcd/refresh-lock "url" "123" 600)
+           false))))
+
+(deftest etcd-success-true
+  (with-redefs [client/put mock-success]
+    (is (= (etcd/refresh-lock "url" "123" 600)
+           true))))
